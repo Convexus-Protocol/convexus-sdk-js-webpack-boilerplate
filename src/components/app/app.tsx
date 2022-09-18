dd5c0e2f49fd704b5d0573333855690c935a39e5
ICONationDevTeam <iconation.team@gmail.com>
2022-09-19 01:55:29 +0200

    Rename modules

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import * as styles from './app.module.less';
-import { ModuleValidateAndParseAddress } from '../modules/validateAndParseAddress/ModuleValidateAndParseAddress';
-import { ModuleInstancePool } from '../modules/instancePool/ModuleInstancePool';
-import { ModuleListOfPools } from '../modules/listOfPools/ModuleListOfPools';
-import { ModuleCreateAndInitializePoolIfNecessary } from '../modules/createAndInitializePoolIfNecessary/ModuleCreateAndInitializePoolIfNecessary';
+import {ModuleValidateAndParseAddress} from '../modules/ModuleValidateAndParseAddress/ModuleValidateAndParseAddress';
+import {ModuleInstancePool} from '../modules/ModuleInstancePool/ModuleInstancePool';
+import {ModuleListOfPools} from '../modules/ModuleListOfPools/ModuleListOfPools';
+import {ModuleCreateAndInitializePoolIfNecessary} from '../modules/ModuleCreateAndInitializePoolIfNecessary/ModuleCreateAndInitializePoolIfNecessary';
 
-export const App = () => 
-  <div className={styles.container}>
-    <ModuleValidateAndParseAddress/>
-    <ModuleInstancePool/>
-    <ModuleListOfPools/>
-    <ModuleCreateAndInitializePoolIfNecessary/>
-  </div>
+export const App = () => (
+    <div className={styles.container}>
+        <ModuleValidateAndParseAddress />
+        <ModuleInstancePool />
+        <ModuleListOfPools />
+        <ModuleCreateAndInitializePoolIfNecessary />
+    </div>
+);
