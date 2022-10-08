9885204b2ad995046636085fdcfa6d7c504eda12
ICONationDevTeam <iconation.team@gmail.com>
2022-10-09 01:27:01 +0200

    Support single & multi quote

--- src/components/modules/ModuleRouting/index.tsx
+++ src/components/modules/ModuleRouting/index.tsx
@@ -2,13 +2,30 @@ import React, {useRef, useState} from 'react';
 
 import * as appStyles from '@components/app/app.module.less';
 import {ModuleHeader} from '@src/components/common/ModuleHeader';
-import {Icx, Token, CurrencyAmount, TradeType} from '@convexus/sdk-core';
-import {QuoteMultiResult, QuoteResult, SwapQuoter, Trade} from '@convexus/sdk';
+import {
+    Icx,
+    Percent,
+    Token,
+    CurrencyAmount,
+    TradeType,
+} from '@convexus/sdk-core';
+import {
+    QuoteMultiResult,
+    QuoteResult,
+    SwapQuoter,
+    SwapRouter,
+    Trade,
+} from '@convexus/sdk';
 import {getAddressFromBookmark} from '@src/components/utils/contract/getAddressFromBookmark';
 import {DefaultFactoryProvider} from '@src/components/utils/contract/ConvexusFactory/getPoolAddress';
 import {quoterContract} from '@src/components/utils/contract/Quoter/getContract';
 import {getTokenFromAddress} from '@src/components/utils/contract/Token/getTokenFromAddress';
 import tryParseCurrencyAmount from '@src/components/utils/parse/tryParseCurrencyAmount';
+import {swapRouterContract} from '@src/components/utils/contract/SwapRouter/getContract';
+import {getUserWallet} from '@src/components/utils/contract/getUserWallet';
+import {getNameContract} from '@src/components/utils/contract/Name/getContract';
+import {TransactionInfo} from '@src/components/common/TransactionInfo';
+import {TxHashLink} from '@src/components/common/TxHashLink';
 
 export function ModuleRouting() {
     const tokenARef = useRef<any>();
@@ -16,6 +33,46 @@ export function ModuleRouting() {
     const amountRef = useRef<any>();
     const [trades, setTrades] = useState<Trade<Token, Token, TradeType>[]>();
     const [amountsOut, setAmountsOut] = useState<CurrencyAmount<Token>[]>();
+    const [txs, setTxs] = useState<TransactionInfo[]>();
+    const [swapIndex, setSwapIndex] = useState<number>();
+
+    const onSwap = async (
+        trade: Trade<Token, Token, TradeType>,
+        index: number,
+    ) => {
+        const wallet = getUserWallet();
+        const slippageTolerance = new Percent(100, 100); // dont care about slippage for testing
+        const recipient = wallet.getAddress();
+        const deadline = Date.now() + 60 * 10; // 10 minute deadline
+
+        const calldatas = SwapRouter.swapCallParameters(trade, {
+            slippageTolerance,
+            recipient,
+            deadline,
+        });
+
+        // deposit tokens & mint the NFT position
+        const txs = [];
+        for (const calldata of calldatas) {
+            const txHash = await swapRouterContract.buildSend(
+                wallet,
+                calldata,
+                true,
+            );
+
+            // Read contract name
+            const nameContract = getNameContract(calldata['to']);
+
+            txs.push({
+                hash: txHash,
+                name: await nameContract.name(),
+                method: calldata['method'],
+            });
+
+            setTxs([...txs]);
+        }
+        setSwapIndex(index);
+    };
 
     const onFindRoute = async () => {
         if (!amountRef.current.value) return;
@@ -61,11 +118,20 @@ export function ModuleRouting() {
                                 return quoterContract
                                     .buildCall(calldata[0])
                                     .then((quoteResult) => {
+                                        // Multi or single QuoteResult ?
+                                        const amountOut =
+                                            trade.swaps[0].route.tokenPath
+                                                .length == 2
+                                                ? QuoteResult.fromCall(
+                                                      quoteResult,
+                                                  ).amountOut
+                                                : QuoteMultiResult.fromCall(
+                                                      quoteResult,
+                                                  ).amountOut;
+
                                         return CurrencyAmount.fromRawAmount(
                                             tokenB,
-                                            QuoteMultiResult.fromCall(
-                                                quoteResult,
-                                            ).amountOut,
+                                            amountOut,
                                         );
                                     });
                             },
@@ -139,6 +205,20 @@ export function ModuleRouting() {
                                 Amount out: {amountsOut[index].toSignificant()}{' '}
                                 {amountsOut[index].currency.symbol}
                             </p>
+                            <p>
+                                <button onClick={() => onSwap(trade, index)}>
+                                    Perform this trade
+                                </button>
+                            </p>
+
+                            {txs &&
+                                swapIndex == index &&
+                                txs.map((tx: any, i: number) => (
+                                    <p key={i}>
+                                        - {tx.name}::{tx.method}
+                                        <TxHashLink txHash={tx.hash} />
+                                    </p>
+                                ))}
                         </p>
                     </>
                 ))}
