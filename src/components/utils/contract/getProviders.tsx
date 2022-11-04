dc43711a08c9a06851634136d75ad6507b4e149f
ICONationDevTeam <iconation.team@gmail.com>
2022-11-04 11:50:38 +0100

    Fix Network ID

--- src/components/utils/contract/getProviders.tsx
+++ src/components/utils/contract/getProviders.tsx
@@ -7,6 +7,6 @@ const debugProvider = new IconService.HttpProvider(
     'https://lisbon.net.solidwallet.io/api/v3d',
 );
 
-export const networkId = 7;
+export const networkId = 2;
 export const iconService = new IconService(httpProvider);
 export const debugService = new IconService(debugProvider);
